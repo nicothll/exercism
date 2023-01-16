54363c4479e3775ca8bea6da5c7c70b4c0e4b6a5
Nicolas Thuillier <nicothll.dev@gmail.com>
2023-01-16 17:11:44 -0600

    Updated yatch exercise

--- python/completed/yacht/yacht.py
+++ python/completed/yacht/yacht.py
@@ -1,4 +1,4 @@
-from collections import Counter
+import collections
 
 # Score categories.
 YACHT = 0
@@ -16,49 +16,39 @@ CHOICE = 13
 
 
 def score(dice: list[int], category: int) -> int:
-    """Given a list of values for 5 dice and a category code
-    Return the score of the dice for that category.
-    If the dice do not satisfy the requirements of the category return 0.
-    Categories Codes:
-    YACHT -> 0, ONES to SIXES -> 1 to 6, FULL_HOUSE -> 7, FOUR_OF_A_KIND -> 10,
-    STRAIGHT: LITTLE -> 11, BIG -> 12, CHOICE -> 13
+    """Function that return the score of a given a list of values for 5 dice.
 
     :param dice: list[int] - given values of 5 dices (1 to 6).
     :param category: int - given category code. See Score categories section,
     :return: int - score
     """
 
-    # Evaluate dice with Counter class from collections package
-    counter = Counter(dice)
-    counter_values = set(counter.values())
-    value, count = counter.most_common(1)[0]
-
-    # ONES to SIXES categories
-    if ONES <= category <= SIXES:
-        return category * dice.count(category)
-
-    # YACHT category
-    if category == YACHT and len(set(dice)) == 1:
-        return 50
+    counter = collections.Counter(dice)
 
     # FULL_HOUSE category (e.g. dice=[1, 1, 3, 3, 3])
-    if category == FULL_HOUSE and counter_values == {2, 3}:
+    if category == FULL_HOUSE and set(counter.values()) == {2, 3}:
         return sum(dice)
 
     # FOUR_OF_A_KIND category (e.g. dice=[5, 2, 2, 2, 2] or dice=[5, 5, 5, 5, 5])
-    if category == FOUR_OF_A_KIND and count >= 4:
-        return 4 * value
+    most_common_nb, most_common_count = counter.most_common(1)[0]
+    if category == FOUR_OF_A_KIND and most_common_count >= 4:
+        return 4 * most_common_nb
 
-    # CHOICE category
-    if category == CHOICE:
-        return sum(dice)
-
-    # LITTLE STRAIGHT categories  (e.g. dice=[1, 2, 3, 4, 5])
+    # LITTLE STRAIGHT category  (e.g. dice=[1, 2, 3, 4, 5])
     if category == LITTLE_STRAIGHT and set(dice) == {1, 2, 3, 4, 5}:
         return 30
 
-    # BIG STRAIGHT categories  (e.g. dice=[2, 3, 4, 5, 6])
+    # BIG STRAIGHT category (e.g. dice=[2, 3, 4, 5, 6])
     if category == BIG_STRAIGHT and set(dice) == {2, 3, 4, 5, 6}:
         return 30
+
+    if ONES <= category <= SIXES:
+        return category * dice.count(category)
+
+    if category == YACHT and len(set(dice)) == 1:
+        return 50
+
+    if category == CHOICE:
+        return sum(dice)
 
     return 0
